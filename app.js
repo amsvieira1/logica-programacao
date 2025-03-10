121cd81ff316da97034ff5c0be03a6f2957dc9be
Alan Vieira <amsvieira1@proton.me>
2025-03-10 16:51:40 -0300

    feat:speak screen messages to the user

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function verificarChute() {
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa} tentativas.`;
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}.`;
         exibirTextoNaTela('p', mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
@@ -45,6 +45,16 @@ function gerarNumeroAleatorio() {
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, "Brazilian Portuguese Female", {rate:1.2});
+    // if ('speechSynthesis' in window) {
+    //     let utterance = new SpeechSynthesisUtterance(texto);
+    //     utterance.lang = 'pt-BR';
+    //     utterance.rate = 1.2;
+    //     window.speechSynthesis.speak(utterance);
+    // } else {
+    //     console.log('Web Speech API não suportado neste navegador.');
+    // }
+
 }
 
 function exibirMensagemInicial() {
